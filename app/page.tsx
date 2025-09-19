5a31e56ab89718660357bae2c4fc5b8b05ad57b7
Pedro Borba <shinkosystems@gmail.com>
2025-09-19 18:34:15 -0300

    Design

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default async function Login({
         const supabase = await createClient();
 
         const { error } = await supabase.auth.signInWithPassword({ email, password });
-        if (error) return redirect("/?message=Não foi possível fazer o login.");
+        if (error) return redirect("/?message=Não foi possível fazer o login!");
         return redirect("/account");
     }
 
